42b56ef5a5cb7ddcf1f71aa525e43cfe208e0632
kenzo1999 <kenzo1999@live.jp>
2015-05-06 11:39:03 +0900

    Git for Extensionsでcommitする
    それからGitHubへもpushする予定

---
+++ codeiqAngou/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeiqAngou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strAngou = "LZH SN STJHZTMNMZQZ JNMN ATMRGNT VN OTQNFTQZLT CD JZHCNJT RHMZRZH "
+                + "RNQDJTQZH CDJHMZJXZ VZSZRH GZ ETSZQH FZ STJHZT MNVN LHSNLDMZHXN";
+            string strAns = "";
+
+            for (int i = 0; i < strAngou.Length; i++)
+            {
+                char ch = strAngou[i];
+                if (ch == 'Z')
+                {
+                    ch = 'A';
+                }
+                else if (ch != ' ')
+                {
+                    ch = (char)((int)ch + 1);
+                }
+                strAns += ch.ToString();
+            }
+            Console.WriteLine("暗号文＝" + strAngou);
+            Console.WriteLine("解読文＝" + strAns);
+            strAns = Console.ReadLine();
+        }
+    }
+}
